3abec972a08b55f8797b9b37be940dfeebea17df
Jayro Martínez Cerveró <43171040+JayroMartinez@users.noreply.github.com>
2021-07-19 14:05:57 +0200

    Creating functions
    - Added new presentations.
    - Started to refactoring the code for dividing it into functions.
    - Now code is more general.

--- load_subject.m
+++ load_subject.m
@@ -1,46 +1,28 @@
 function [out_data] = load_subject(subject_to_load)
 
-% LOADSUBJECT Function to load data corresponding to a single subject.
+% LOAD_SUBJECT Function to load data corresponding to a single subject.
 %
 % INPUT
 % subject_to_load:  String with the subject's name.
 %
 % OUTPUT
-% out_data:         Data from the corresponding subject.
+% out_data:         Kinematic data from the corresponding subject.
 %
 % AUTHOR:           Jayro Martinez-Cervero
 % CREATED:          24/06/21
-% LAST MODIFIED:    24/06/21
+% LAST MODIFIED:    19/07/21
 
-path = fullfile(pwd,'Data',subject_to_load,'haptic_exploration');
+file = [pwd,'/Data/', 'Data_', subject_to_load, '.mat'];
 
-old_dir = pwd;
-d = dir(path); 
-isub = [d(:).isdir]; 
-nameFolds = string({d(isub).name});
-nameFolds = nameFolds(nameFolds ~= '.');
-nameFolds = nameFolds(nameFolds ~= '..');
-cd(old_dir);
+aux_data = load(file);
 
-folders_to_exclude = {'calibration', 'Calibration', 'CALIBRATION'};
+out_data = [];
 
-subjects_data = [];
+for i = 1:numel(aux_data.haptic_exploration_data.subjects.tasks)
 
-for i = 1:numel(nameFolds)
-    
-    if ~ismember(nameFolds(i),folders_to_exclude);
-        
-        file = fullfile(path,nameFolds(i),'_cyberglove_calibrated_joint_states.csv');
-    
-        aux_data = readtable(file);
-        subjects_data = [subjects_data; aux_data];
-        
-        clear aux_data;
-    end
+    out_data = [out_data; aux_data.haptic_exploration_data.subjects.tasks(i).data(1).data(:,2:end)];
+   
 end
 
-out_data = subjects_data;
-
-clear subjects_data;
 
 end
