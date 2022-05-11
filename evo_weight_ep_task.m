0d83a1a3fa4675f4ec684d401f31d6ccc47ddb5a
Jayro Martínez Cerveró <43171040+JayroMartinez@users.noreply.github.com>
2022-05-11 11:37:40 +0200

    Included Simon Files
    - Add filter_emg to filter emg data
    - Add SGraw_data to show a "movie" with hand positions

--- evo_weight_ep_task.m
+++ evo_weight_ep_task.m
@@ -6,8 +6,8 @@ function evo_weight_ep_task(sorted_scores, sorted_pcs, means, ep_labels, task_la
 colors = [[1 0 0]; [0 1 0]; [0 0 1]; [0.3010 0.7450 0.9330]; [1 0 1]; [1 1 0]; [0 0 0]; [0 0.4470 0.7410]; [0.8500 0.3250 0.0980]; [0.9290 0.6940 0.1250]; [0.4940 0.1840 0.5560]; [0.4660 0.6740 0.1880]; [0 1 1]; [0.6350 0.0780 0.1840]; [0.47 0.25 0.80]; [0.25 0.80 0.54]; [1.00 0.54 0.00]; [0.83 0.14 0.14]; [0.5 0.5 0.5]];
 
 close all;
-task_to_select ='Fork_';
-ep_to_select ='translation';
+task_to_select ='Ball';
+ep_to_select ='rotation';
 % number_synergies = 5;
 
 resampled_scores = cell(size(sorted_scores,1),size(sorted_scores,2));
