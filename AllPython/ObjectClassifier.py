696da2960995ac1097888818246fbfdc96f6e757
JayroMartinez <jaymarcer@gmail.com>
2022-10-20 15:12:43 +0200

    Initial commit on new python data
    - New code, all process in a unique pipeline in python

---
+++ AllPython/ObjectClassifier.py
@@ -0,0 +1,15 @@
+import os
+
+from load_subject import load
+
+def main():
+
+    data_folder = '/BIDSData'
+    subject_folders = sorted([f.path for f in os.scandir(os.getcwd() + data_folder) if f.is_dir()])
+
+    for subject in subject_folders:
+        load(subject)
+
+
+if __name__ == "__main__":
+        main()
