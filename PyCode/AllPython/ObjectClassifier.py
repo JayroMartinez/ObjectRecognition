803f0a51d839853fe7a71d7c8dadf6590e6c7e55
JayroMartinez <jaymarcer@gmail.com>
2022-11-25 18:03:41 +0100

    Update ObjectClassifier.py

--- PyCode/AllPython/ObjectClassifier.py
+++ PyCode/AllPython/ObjectClassifier.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import time
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 pd.options.mode.chained_assignment = None
@@ -25,9 +26,18 @@ def main():
     split_df['Trial num'] = split_df['Trial num'].astype('str')
     split_df['EP num'] = split_df['EP num'].astype('str')
 
-    # emg_classification(split_df)
-    # kinematic_classification(split_df)
+    init_time = time.time()
+    emg_classification(split_df)
+    emg_time = time.time()
+    print("EMG elapsed time: ", round(emg_time - init_time))
+    kinematic_classification(split_df)
+    kinematic_time = time.time()
+    print("Kinematic elapsed time: ", round(kinematic_time - emg_time))
     multiple_source_classification(split_df)
+    multimodal_time = time.time()
+    print("Kinematic elapsed time: ", round(multimodal_time - kinematic_time))
+    print("###########################################")
+    print("TOTAL elapsed time: ", round(multimodal_time - init_time))
 
 
 if __name__ == "__main__":
